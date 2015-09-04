cb0595714ee517ce18230655666c02d8f9a2e19d
cguan <cguan@loyalty.com>
2015-09-04 10:28:02 -0400

    first fizzBuzzTest passing, checks value of 1 returns1

--- src/test/java/com/letsdeveloper/kata/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/letsdeveloper/kata/fizzbuzz/FizzBuzzTest.java
@@ -1,51 +1,16 @@
 package com.letsdeveloper.kata.fizzbuzz;
 
-import org.hamcrest.Matcher;
 import org.junit.Test;
+import org.junit.Assert;
+import org.hamcrest.CoreMatchers;
 
-import static org.hamcrest.CoreMatchers.is;
-
-import static org.junit.Assert.assertThat;
-
+/**
+ * Created by cguan on 04/09/2015.
+ */
 public class FizzBuzzTest {
-
-	@Test
-	public void shouldReturnNumberIfNotDevisibleByThreeAndFive() {
-		assertThatAnswerFor(1, is("1"));
-		assertThatAnswerFor(2, is("2"));
-		assertThatAnswerFor(4, is("4"));
-		assertThatAnswerFor(7, is("7"));
-	}
-
-	@Test
-	public void shouldReturnFizzForMultiplesOfThree() {
-		assertThatAnswerFor(3, is("Fizz"));
-		assertThatAnswerFor(6, is("Fizz"));
-	}
-
-	@Test
-	public void shouldReturnBuzzForMultiplesOfFive() {
-		assertThatAnswerFor(5, is("Buzz"));
-		assertThatAnswerFor(10, is("Buzz"));
-	}
-
-	@Test
-	public void shouldReturnFizzBuzzForMultiplesOfThreeAndFive() {
-		assertThatAnswerFor(15, is("FizzBuzz"));
-		assertThatAnswerFor(30, is("FizzBuzz"));
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void shouldThrowForZero() {
-		FizzBuzz.getAnswerFor(0);
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void shouldThrowForMinus1() {
-		FizzBuzz.getAnswerFor(-1);
-	}
-
-	private void assertThatAnswerFor(int number, Matcher<String> matcher) {
-		assertThat(FizzBuzz.getAnswerFor(number), matcher);
-	}
+    @Test
+    public void shouldReturnOneaAsNUmber(){
+        String answer = FizzBuzz.getAnswerFor(1);
+        Assert.assertThat(answer, CoreMatchers.is("1"));
+    }
 }
